51f4adebdcee4332f1d75ea9c2505787c4eb7256
ashwinishankwaar <ashwinishankwaar@gmail.com>
2026-08-08 15:17:06 +0530

    committing the solution of question Two Sum

--- solutions/two_sum/solution.py
+++ solutions/two_sum/solution.py
@@ -6,20 +6,6 @@ class Solution:
         pass
 
     def _run(self, nums: List[int], target: int):
-        # sort indices by value so the two-pointer scan works but we can
-        # still return the ORIGINAL indices (sorting nums directly loses them)
-        # order = sorted(range(len(nums)), key=lambda k: nums[k])
-        # i = 0
-        # j = len(nums) - 1  # last valid index, not len(nums)
-        # while i < j:
-        #     total = nums[order[i]] + nums[order[j]]  # 'sum' shadows the builtin
-        #     if total == target:
-        #         return sorted([order[i], order[j]])
-        #     elif total > target:
-        #         j -= 1  # 'j = - 1' assigned -1 instead of decrementing
-        #     else:
-        #         i += 1  # same bug: 'i = + 1' assigned +1
-        # return []
         nums_dict = {}
         for i in range(len(nums)):
             c = target-nums[i]
